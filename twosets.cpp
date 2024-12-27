fd502063721cf099090f2bc72e782c39a344c1ae
Naman <140693717+newman05@users.noreply.github.com>
2024-12-27 18:16:15 +0530

    Add files via upload

---
+++ twosets.cpp
@@ -0,0 +1,33 @@
+
+
+// #include<bits/stdc++.h>
+
+// #define ll long long
+// #define nline '\n' 
+// #define FOR for(int i = 0 ;i < n ; i++) cin>>v[i];
+// #define nv int n ; cin>>n ; vector<int>v(n) ;
+// #define SORT sort(v.begin() , v.end()) ;
+// #define stinsert st.insert(v.begin() , v.end());
+
+// using namespace std ;
+
+// int main(){ ;
+// ios_base::sync_with_stdio(false);
+// cin.tie(NULL);
+
+// int n ;
+// cin>>n ;
+
+// if( n % 2 == 0 ) {
+//   cout<<"NO"<<nline ;
+// }
+// else {
+//   cout<<"YES"<<nline ;
+//   cout<<n/2<<nline ;
+  
+// }
+
+
+
+// return 0 ;
+// }
